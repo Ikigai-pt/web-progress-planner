5219bcf3c888d3ac38656531af24c0fb53988f6d
pthirunavukkarasu <pthirunavukkarasu@prosper.com>
2018-03-03 19:39:13 -0800

    fixed mock data ids

--- app/containers/HomePage/Features/TaskListPanel/index.js
+++ app/containers/HomePage/Features/TaskListPanel/index.js
@@ -32,13 +32,13 @@ class TimelinePanel extends React.Component {
   render() {
     const tasks = [
       { id: 1, title: 'Go to gym', desc: '  Abs workout  fjsldfkfskfs dkfljlkkjldshuffhifurf fjsdffuwybfasHDfbfhb fsfdsjfs Abs workout  fjsldfkfskfs dkfljlkkjldshuffhifurf fjsdffuwybfasHDfbfhb fsfdsjfs ', date: new Date() },
-      { id: 1, title: 'Go to gym', desc: '  Abs workout  fjsldfkfskfs dkfljlkkjldshuffhifurf fjsdffuwybfasHDfbfhb fsfdsjfs Abs workout  fjsldfkfskfs dkfljlkkjldshuffhifurf fjsdffuwybfasHDfbfhb fsfdsjfs ', date: new Date() },
-      { id: 1, title: 'Go to gym', desc: '  Abs workout  fjsldfkfskfs dkfljlkkjldshuffhifurf fjsdffuwybfasHDfbfhb fsfdsjfs Abs workout  fjsldfkfskfs dkfljlkkjldshuffhifurf fjsdffuwybfasHDfbfhb fsfdsjfs ', date: new Date() },
-      { id: 1, title: 'Go to gym', desc: '  Abs workout  fjsldfkfskfs dkfljlkkjldshuffhifurf fjsdffuwybfasHDfbfhb fsfdsjfs Abs workout  fjsldfkfskfs dkfljlkkjldshuffhifurf fjsdffuwybfasHDfbfhb fsfdsjfs ', date: new Date() },
-      { id: 1, title: 'Go to gym', desc: '  Abs workout  fjsldfkfskfs dkfljlkkjldshuffhifurf fjsdffuwybfasHDfbfhb fsfdsjfs Abs workout  fjsldfkfskfs dkfljlkkjldshuffhifurf fjsdffuwybfasHDfbfhb fsfdsjfs ', date: new Date() },
-      { id: 1, title: 'Go to gym', desc: '  Abs workout  fjsldfkfskfs dkfljlkkjldshuffhifurf fjsdffuwybfasHDfbfhb fsfdsjfs Abs workout  fjsldfkfskfs dkfljlkkjldshuffhifurf fjsdffuwybfasHDfbfhb fsfdsjfs ', date: new Date() },
-      { id: 1, title: 'Go to gym', desc: '  Abs workout  fjsldfkfskfs dkfljlkkjldshuffhifurf fjsdffuwybfasHDfbfhb fsfdsjfs Abs workout  fjsldfkfskfs dkfljlkkjldshuffhifurf fjsdffuwybfasHDfbfhb fsfdsjfs ', date: new Date() },
-      { id: 1, title: 'Go to gym', desc: '  Abs workout  fjsldfkfskfs dkfljlkkjldshuffhifurf fjsdffuwybfasHDfbfhb fsfdsjfs Abs workout  fjsldfkfskfs dkfljlkkjldshuffhifurf fjsdffuwybfasHDfbfhb fsfdsjfs ', date: new Date() }];
+      { id: 2, title: 'Go to gym', desc: '  Abs workout  fjsldfkfskfs dkfljlkkjldshuffhifurf fjsdffuwybfasHDfbfhb fsfdsjfs Abs workout  fjsldfkfskfs dkfljlkkjldshuffhifurf fjsdffuwybfasHDfbfhb fsfdsjfs ', date: new Date() },
+      { id: 3, title: 'Go to gym', desc: '  Abs workout  fjsldfkfskfs dkfljlkkjldshuffhifurf fjsdffuwybfasHDfbfhb fsfdsjfs Abs workout  fjsldfkfskfs dkfljlkkjldshuffhifurf fjsdffuwybfasHDfbfhb fsfdsjfs ', date: new Date() },
+      { id: 4, title: 'Go to gym', desc: '  Abs workout  fjsldfkfskfs dkfljlkkjldshuffhifurf fjsdffuwybfasHDfbfhb fsfdsjfs Abs workout  fjsldfkfskfs dkfljlkkjldshuffhifurf fjsdffuwybfasHDfbfhb fsfdsjfs ', date: new Date() },
+      { id: 5, title: 'Go to gym', desc: '  Abs workout  fjsldfkfskfs dkfljlkkjldshuffhifurf fjsdffuwybfasHDfbfhb fsfdsjfs Abs workout  fjsldfkfskfs dkfljlkkjldshuffhifurf fjsdffuwybfasHDfbfhb fsfdsjfs ', date: new Date() },
+      { id: 6, title: 'Go to gym', desc: '  Abs workout  fjsldfkfskfs dkfljlkkjldshuffhifurf fjsdffuwybfasHDfbfhb fsfdsjfs Abs workout  fjsldfkfskfs dkfljlkkjldshuffhifurf fjsdffuwybfasHDfbfhb fsfdsjfs ', date: new Date() },
+      { id: 7, title: 'Go to gym', desc: '  Abs workout  fjsldfkfskfs dkfljlkkjldshuffhifurf fjsdffuwybfasHDfbfhb fsfdsjfs Abs workout  fjsldfkfskfs dkfljlkkjldshuffhifurf fjsdffuwybfasHDfbfhb fsfdsjfs ', date: new Date() },
+      { id: 8, title: 'Go to gym', desc: '  Abs workout  fjsldfkfskfs dkfljlkkjldshuffhifurf fjsdffuwybfasHDfbfhb fsfdsjfs Abs workout  fjsldfkfskfs dkfljlkkjldshuffhifurf fjsdffuwybfasHDfbfhb fsfdsjfs ', date: new Date() }];
 
     return (
       <TasksAndHabitsWrapper>
